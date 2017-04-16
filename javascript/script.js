c1343e33c597110bbcf4c6ca941d2a458957bce8
Coffeeteer <tgrieshiemer@gmail.com>
2017-04-15 22:27:25 -0400

    created a server file

--- javascript/script.js
+++ javascript/script.js
@@ -10,9 +10,7 @@ $(document).ready(function() {
 	var month_name = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 	var day_name = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday'];
 
-	
 	console.log(month_name[month], 'This Month');
 	$('#present-month-year').append(month_name[month] + ' ' + new Date().getFullYear());
 
-	
 });
