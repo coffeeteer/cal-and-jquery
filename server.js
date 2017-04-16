24a306d8bef1e7d4d9c0616bae326b70de757063
Coffeeteer <tgrieshiemer@gmail.com>
2017-04-16 19:00:43 -0400

    finally got the server to work

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express        = require('express');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
+var cookieParser   = require('cookie-parser');
+var http           = require('http');
+var path           = require('path');
 var moment         = require('moment');
 moment().format();
 var app = express();
@@ -13,13 +16,20 @@ var port = process.env.PORT || 3010;
 app.use(express.static(process.cwd() + '/public'));
 
 app.use(bodyParser.urlencoded({
-    extended: false
+    extended: true
 }));
 
-app.use(methodOverride('_method'));
+app.use(express.static(process.cwd() ));
+
+// app.set('view engine', 'http');
 
 
 /*---------- Routes ------------*/
-app.get('/', function(req, res) {
-    res.render('index.html');
+app.get('/', function (req, res) {
+  res.render('views/index.html')
+})
+
+/*----- Listen -----*/
+app.listen(port, function() {
+    console.log('connected to port ', port);
 });
