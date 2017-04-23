7376ba0e6e7d6ea70b7c00d41b93931572e1d179
Coffeeteer <tgrieshiemer@gmail.com>
2017-04-22 23:04:47 -0400

    adding events into the JavaScript

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,10 +1,26 @@
 $(document).ready(function(){
 	$('#calendar').fullCalendar({
         // put your options and callbacks here
+        //height: 650,
+        aspectRatio: 2,
         header: {
                 left: ' today,prev,next,  title',
-                center: '',
                 right: 'month,agendaWeek,agendaDay '
-            }
+        },
+        businessHours: [ 
+        	{
+			    // days of week. an array of zero-based day of week integers (0=Sunday)
+			    dow: [ 1, 2, 3, 4, 5], // Monday - Friday
+
+			    start: '08:00', // a start time (8am in this example)
+			    end: '16:00', // an end time (4pm in this example)
+			},
+			{
+				dow: [6, 0],
+				start: '12:00', // a start time (12pm in this example)
+			    end: '00:00', // an end time (12am in this example)	
+			}
+		 ],
+		 events: [],
     }); //calendar wrap
 }); //document.ready
