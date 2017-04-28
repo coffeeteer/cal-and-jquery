a689e5d4321d064068c422e1dcdf2b6c830350ad
Coffeeteer <tgrieshiemer@gmail.com>
2017-04-27 21:34:16 -0400

    minor changes

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -13,28 +13,7 @@ $(document).ready(function(){
 	        // textColor: black ,// an option!
 
 	        events: [
-		        {
-		            title          : 'Day Shift',
-		            start          : '08:00',
-		            end            :  '16:00',
-		            //rendering      : 'background',
-		            textColor      : 'black' ,// an option!
-		            backgroundColor: 'orange',
-		            description    : 'Day Shift',
-		            allDay : false
-		            
-		        },
-		        {   
-		            title          : 'Afternoon Shift',
-		            start          : '16:00',
-		            end            :  '00:00',
-		            //rendering      : 'background',
-		            textColor      : 'black' ,// an option!
-		            backgroundColor: 'red',
-		            description    : 'Afternoon Shift',
-		            allDay : false
-		        },
-		        {
+	        	{
 		         	title          : 'Midnight Shift',
 		            start          : '00:00',
 		            end            :  '08:00',
@@ -42,15 +21,33 @@ $(document).ready(function(){
 		            textColor      : 'black' ,// an option!
 		            backgroundColor: 'blue',
 		            description    : 'Midnight Shift',   
-		            allDay : false // will make the time show
+		            allDay         : false // will make the time show
+		        },
+		        {
+		            title          : 'Day Shift',
+		            start          : '08:00',
+		            end            :  '16:00',
+		            //rendering      : 'background',
+		            textColor      : 'black' ,// an option!
+		            backgroundColor: 'yellow',
+		            description    : 'Day Shift',
+		            allDay         : false
+		            
+		        },
+		        {   
+		            title          : 'Afternoon Shift',
+		            start          : '16:00',
+		            end            : '24:00',
+		            // rendering      : 'background',
+		            textColor      : 'black' ,// an option!
+		            backgroundColor: 'orange',
+		            description    : 'Afternoon Shift',
+		            allDay         : false
 		        }
 		    ]
-
-			
+		     
 	        
 	    }); //calendar wrap function
 
-
-$(document).on('load', calendar.eventRender);
-//$(document).on('turbolinks:load', initialize_calendar);	//Event handler
+//$(document).on('load', calendar.eventRender);
 }); //document.ready
