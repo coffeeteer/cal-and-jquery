262b48a608e2db86e90b7503f2251dfe0a4b92dd
Coffeeteer <tgrieshiemer@gmail.com>
2017-04-27 21:55:51 -0400

    failed experiment, try again tomorrow

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -13,37 +13,51 @@ $(document).ready(function(){
 	        // textColor: black ,// an option!
 
 	        events: [
-	        	{
-		         	title          : 'Midnight Shift',
-		            start          : '00:00',
+	        	// {
+		        //  	title          : 'Midnight Shift',
+		        //     start          : '00:00',
+		        //     end            :  '08:00',
+		        //     //rendering      : 'background',
+		        //     textColor      : 'black' ,// an option!
+		        //     backgroundColor: 'blue',
+		        //     description    : 'Midnight Shift',   
+		        //     allDay         : false // will make the time show
+		        // },
+		        // {
+		        //     title          : 'Day Shift',
+		        //     start          : '08:00',
+		        //     end            :  '16:00',
+		        //     //rendering      : 'background',
+		        //     textColor      : 'black' ,// an option!
+		        //     backgroundColor: 'yellow',
+		        //     description    : 'Day Shift',
+		        //     allDay         : false
+		            
+		        // },
+		        // {   
+		        //     title          : 'Afternoon Shift',
+		        //     start          : '16:00',
+		        //     end            : '24:00',
+		        //     // rendering      : 'background',
+		        //     textColor      : 'black' ,// an option!
+		        //     backgroundColor: 'orange',
+		        //     description    : 'Afternoon Shift',
+		        //     allDay         : false
+		        // },
+		    ],
+		    events: [
+		    	{
+		    		title          : 'Scott Walsh',
+		    		description    : 'Midnight Shift',
+		    		allDay         : false,
+	    			start          : '00:00',
 		            end            :  '08:00',
 		            //rendering      : 'background',
 		            textColor      : 'black' ,// an option!
 		            backgroundColor: 'blue',
-		            description    : 'Midnight Shift',   
-		            allDay         : false // will make the time show
-		        },
-		        {
-		            title          : 'Day Shift',
-		            start          : '08:00',
-		            end            :  '16:00',
-		            //rendering      : 'background',
-		            textColor      : 'black' ,// an option!
-		            backgroundColor: 'yellow',
-		            description    : 'Day Shift',
-		            allDay         : false
-		            
-		        },
-		        {   
-		            title          : 'Afternoon Shift',
-		            start          : '16:00',
-		            end            : '24:00',
-		            // rendering      : 'background',
-		            textColor      : 'black' ,// an option!
-		            backgroundColor: 'orange',
-		            description    : 'Afternoon Shift',
-		            allDay         : false
-		        }
+	            	allDay         : false, // will make the time show
+	            	dow            : [ 1, 2, 3, 4, 5 ]
+		    	}
 		    ]
 		     
 	        
