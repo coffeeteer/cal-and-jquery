b9e9b50fb56fda12242271054fbc7ee3ffd894d5
Coffeeteer <tgrieshiemer@gmail.com>
2017-06-25 22:18:28 -0400

    database created but not working ---meh

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,22 +1,26 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-	var events = [
-		{id: 1, text: 'Travis G', start_date: '2017/06/16 16:00', end_date: '2017/06/17 00:00'},
-		{id: 2, text: 'Glenn H', start_date: '2017/06/16 16:15', end_date: '2017/06/17 08:00'}
-	];
+// 	// var events = [
+// 	// 	{id: 1, text: 'Travis G', start_date: '2017/06/16 16:00', end_date: '2017/06/17 00:00'},
+// 	// 	{id: 2, text: 'Glenn H', start_date: '2017/06/16 16:15', end_date: '2017/06/17 08:00'}
+// 	// ];
 
-	(function init() {
-		scheduler.config.xml_date='%Y-%m-%d %H:%i';
+// 	(function init() {
+// 		scheduler.config.xml_date='%Y-%m-%d %H:%i';
 
-    	scheduler.init('scheduler_here', new Date(), 'month'); // Starts with today's date
+//         var dp = new dataProcessor("/data");
+//         dp.init(scheduler);
+//         dp.setTransactionMode("POST", false);
 
-	    scheduler.parse(events, 'json');
-	    scheduler.parse('/data', 'json');
-    })();
+// 	    scheduler.init('scheduler_here', new Date(2017,8,4),"month");
 
-	con.connect(function(err) {
-	  if (err) throw err;
-	  console.log('Connected!');
-	});
+//         scheduler.templates.xml_date = function(value){ return new Date(value); };
+//         scheduler.load("/data", "json");
+//     })();
+
+// 	con.connect(function(err) {
+// 	  if (err) throw err;
+// 	  console.log('Connected!');
+// 	});
     
-}); //Document.ready
+// }); //Document.ready
--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var path = require('path');
 var Promise = require('es6-promise').Promise;
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mysql = require('mysql');
 
-// var index = require('./models/index.js');
+var db = require('mongoskin').db("mongodb://localhost:27017/cogen", { w: 0});
+    db.bind('event');
 
 const app = express();
 
@@ -21,28 +21,84 @@ app.use(logger('dev', {
   skip: function (req, res) { return res.statusCode < 400 }
 }));
 
+/*-----------------Event Handlers---------------------*/
+
+//Adds records to the database
+app.get('/init', function(req, res){
+
+    db.event.insert({ 
+        text:"My test event A", 
+        start_date: new Date(2017,5,25),
+        end_date:   new Date(2017,5,26)
+    });
+    db.event.insert({ 
+        text:"One more test event", 
+        start_date: new Date(2017,5,5),
+        end_date:   new Date(2017,5,6),
+        color: "#BADA55"
+    });
+
+    /*... skipping similar code for other test events...*/
+
+    res.send("Test events were added to the database")
+});
+
+//Loads data from the database
+app.get('/data', function(req, res){
+    db.event.find().toArray(function(err, data){
+        //set id property for all records
+        for (var i = 0; i < data.length; i++)
+            data[i].id = data[i]._id;
+
+        //output response
+        res.send(data);
+    });
+});
+
+app.post('/data', function(req, res){
+    var data = req.body;
+
+    //get operation type
+    var mode = data["!nativeeditor_status"];
+    //get id of record
+    var sid = data.id;
+    var tid = sid;
+
+    //remove properties which we do not want to save in DB
+    delete data.id;
+    delete data.gr_id;
+    delete data["!nativeeditor_status"];
+
+
+    //output confirmation response
+    function update_response(err, result){
+        if (err)
+            mode = "error";
+        else if (mode == "inserted")
+            tid = data._id;
+
+        res.setHeader("Content-Type","text/xml");
+        res.send("<data><action type='"+mode+"' sid='"+sid+"' tid='"+tid+"'/></data>");
+    }
+
+    //run db operation
+    if (mode == "updated")
+        db.event.updateById( sid, data, update_response);
+    else if (mode == "inserted")
+        db.event.insert(data, update_response);
+    else if (mode == "deleted")
+        db.event.removeById( sid, update_response);
+    else
+        res.send("Not supported operation");
+});
+/*-----------------Event Handlers---------------------*/
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + 'index.html');
 });
 
 //DHX Calendar **********************
 
-/*Using MySQL for calendar*/
-app.get('/data', function(req, res){
-    console.log('hitting app.get("data")');
-    connection.event.find().toArray(function(err, data){
-        console.log('going through connection event');
-        //set id property for all records
-        for (var i = 0; i < rows.length; i++)
-            rows[i].text;
-            console.log('hitting app.get("data") for loop');
-        //output response
-        res.send(data);
-    });
-});
 
-
-/*Using MySQL for calendar*/
 //DHX Calendar ****************************************
 
 const server = new http.Server(app);
