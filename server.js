0e74eef5e3cb21b9226765d4c881ea60f0678f8f
Coffeeteer <tgrieshiemer@gmail.com>
2017-06-30 21:24:39 -0400

    meh

--- server.js
+++ server.js
@@ -72,8 +72,9 @@
 
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 
-var db = require('mongoskin').db("localhost/testdb", { w: 0});
+var db = require('mongoskin').db("mongodb://localhost:27017/testdb", { w: 0});
     db.bind('event');
 
 
@@ -83,11 +84,13 @@ app.use(express.bodyParser());
 
 
 app.get('/init', function(req, res){
+    if (err) throw err;
+    console.log('before object');
     db.event.insert({ 
         text:"My test event A", 
         start_date: new Date(2017,5,1),
         end_date:   new Date(2017,5,5)
-    });
+    }, console.log('first object run'));
     db.event.insert({ 
         text:"My test event B", 
         start_date: new Date(2017,5,19),
@@ -104,53 +107,22 @@ app.get('/init', function(req, res){
         end_date:   new Date(2017,5,8),
         color: "#DD8616"
     });
-
-    res.send("Test events were added to the database")
+    res.send("Test events were added to the database");
+    console.log('events inserted?')
 });
 
 app.get('/data', function(req, res){
     db.event.find().toArray(function(err, data){
         //set id property for all records
         for (var i = 0; i < data.length; i++)
+            // console.log('in for loop');
             data[i].id = data[i]._id;
-            console.log(data)
-        
+
         //output response
         res.send(data);
+        console.log('data sent');
     });
 });
 
-
-app.post('/data', function(req, res){
-    var data = req.body;
-    var mode = data["!nativeeditor_status"];
-    var sid = data.id;
-    var tid = sid;
-
-    delete data.id;
-    delete data.gr_id;
-    delete data["!nativeeditor_status"];
-
-
-    function update_response(err, result){
-        if (err)
-            mode = "error";
-        else if (mode == "inserted")
-            tid = data._id;
-
-        res.setHeader("Content-Type","text/xml");
-        res.send("<data><action type='"+mode+"' sid='"+sid+"' tid='"+tid+"'/></data>");
-    }
-
-    if (mode == "updated")
-        db.event.updateById( sid, data, update_response);
-    else if (mode == "inserted")
-        db.event.insert(data, update_response);
-    else if (mode == "deleted")
-        db.event.removeById( sid, update_response);
-    else
-        res.send("Not supported operation");
-});
-
 app.listen(3000);
-console.log('listening on port 3000')
+console.log('listening on port 3000');
