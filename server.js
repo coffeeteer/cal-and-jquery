97528d1129581393bbe575dc79fba3ea4bf43e83
Coffeeteer <tgrieshiemer@gmail.com>
2017-07-06 21:03:18 -0400

    testing still testing

--- server.js
+++ server.js
@@ -1,75 +1,3 @@
-// var http = require('http');
-// var express = require('express');
-// // var logger = require('morgan');
-// var path = require('path');
-// // var Promise = require('es6-promise').Promise;
-// // var cookieParser = require('cookie-parser');
-// // var bodyParser = require('body-parser');
-
-// var db = require('mongoskin').db("mongodb://localhost:27017/cogen", { w: 0});
-//     db.bind('event');
-
-// const app = express();
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join('./public')));
-
-// // app.use(logger('dev', {
-// //   skip: function (req, res) { return res.statusCode < 400 }
-// // }));
-// //DHX Calendar ****************************************START
-// /*-----------------Event Handlers---------------------*/
-
-// //Adds records to the database
-// app.get('/init', function(req, res){
-//     scheduler.config.xml_date='%Y-%m-%d %H:%i';
-//     console.log('anything?')
-//     db.createCollection(event, console.log('createCollection fired'));
-
-//     db.event.insert({ 
-//         text:'Person 1', 
-//         start_date: new Date(2017,5,25, 0800),
-//         end_date:   new Date(2017,5,26, 1600),
-//         color: '#eee'
-//     });
-//     db.event.insert({ 
-//         text:'Person 2', 
-//         start_date: new Date(2017,5,5, 1600),
-//         end_date:   new Date(2017,5,6, 2359),
-//         color: "#BADA55"
-//     });
-
-//     /*... skipping similar code for other test events...*/
-
-//     res.send("Test events were added to the database")
-// });
-
-// //Loads data from the database
-// app.get('/data', function(req, res){
-//     db.event.find().toArray(function(err, data){
-//         for (var i = 0; i < data.length; i++)
-//             data[i].id = data[i]._id;
-
-//         //output response
-//         res.send(data);
-//     });
-// });
-// /*-----------------Event Handlers---------------------*/
-// //DHX Calendar ****************************************END
-
-// app.get('/', (req, res) => {
-// 	res.sendFile(__dirname + 'index.html');
-// });
-
-// const server = new http.Server(app);
-
-// const port = process.env.PORT || 3010;
-// server.listen(port, () => {
-// 	console.log(`connected to port ${port}`);
-// });
-
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
@@ -89,23 +17,23 @@ app.get('/init', function(req, res){
     console.log('before object');
     db.event.insert({ 
         text:"My test event A", 
-        start_date: new Date(2017,5,1),
-        end_date:   new Date(2017,5,5)
+        start_date: new Date(2017,6,1),
+        end_date:   new Date(2017,6,5)
     }, console.log('first object run'));
     db.event.insert({ 
         text:"My test event B", 
-        start_date: new Date(2017,5,19),
-        end_date:   new Date(2017,5,24)
+        start_date: new Date(2017,6,19),
+        end_date:   new Date(2017,6,24)
     });
     db.event.insert({ 
         text:"Morning event", 
-        start_date: new Date(2017,5,4),
-        end_date:   new Date(2017,5,4)
+        start_date: new Date(2017,6,4),
+        end_date:   new Date(2017,6,4)
     });
     db.event.insert({ 
         text:"One more test event", 
-        start_date: new Date(2017,5,3),
-        end_date:   new Date(2017,5,8),
+        start_date: new Date(2017,6,3),
+        end_date:   new Date(2017,6,8),
         color: "#DD8616"
     });
     res.send("Test events were added to the database");
@@ -155,6 +83,8 @@ app.post('/data', function(req, res){
     else
         res.send("Not supported operation");
 });
+
+EDITOR="C\\PROGRA~1\\SUBLIM~1\\sublime-text.exe"
 
 
 app.listen(3000);
